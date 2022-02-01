83afaca918018d2d7f9bfdee4c8e4c50839dd23f
alya222 <alya_nimis-ibrahim@isd31.net>
2022-02-01 16:03:29 -0600

    changed .andThen to .alongWith in moveArm RunCommand

--- src/main/java/frc/robot/subsystems/PIDClimb.java
+++ src/main/java/frc/robot/subsystems/PIDClimb.java
@@ -8,14 +8,17 @@ import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.PIDSubsystem;
 import frc.robot.subsystems.ClimbAuto;
 
-//private final ClimbAuto climbAuto;
 
 public class PIDClimb extends PIDSubsystem {
+
+  private ClimbAuto climbAuto;
+
   /** Creates a new PIDClimb. */
-  public PIDClimb(ClimbAuto climb) {
+  public PIDClimb(ClimbAuto cAuto) {
     super(
         // The PIDController used by the subsystem
         new PIDController(0.1, 0, 0));
+    this.climbAuto = cAuto;
   }
 
   @Override

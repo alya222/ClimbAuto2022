359a08066f73d27af22bc03ce57944e95a8baa35
alya222 <alyanimisibrahim@gmail.com>
2022-02-10 15:36:29 -0600

    minor formatting change

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -15,6 +15,7 @@ import edu.wpi.first.wpilibj.PneumaticsModuleType;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
+
 public final class Constants {
     
     // assign port number to lift motor
